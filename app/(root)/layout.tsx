3d90ae3520520f4fe8bbdde651fe09d0b209d261
NCDinesh <deenez.npl1@gmail.com>
2024-10-30 14:41:56 +0545

    navbar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const layout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <main className='font-work-sans'>
+      <Navbar>  </Navbar>
+      {children}
+    </main>
+  )
+}
+
+export default layout
