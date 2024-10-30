ca5a4207c64bbdb43d0572d5cd7fe204428c58a2
NCDinesh <deenez.npl1@gmail.com>
2024-10-30 21:52:05 +0545

    Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home () {
   return (
     <>
-    Home
+    <section className="pink_container">
+    <h1 className="heading ">Pitch Your startup ,<br/> Connect with enterpreneurs</h1>
+
+    <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual</p>
+    </section>
+    
     </>
   )
 }
